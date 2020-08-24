50a90463db546bcb5f00e785e38fb8b605179c84
Osinachi Nwogu <nicknwogu99@gmail.com>
2020-08-24 08:29:47 -0500

    [feat] Added test for the factories

---
+++ tests/FactoryTest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace tests;
+
+use Factories\PlaneTransporterFactory;
+use Factories\ShipTransporterFactory;
+use Factories\TruckTransporterFactory;
+use PHPUnit\Framework\TestCase;
+use Transporters\PlaneTransporter;
+use Transporters\ShipTransporter;
+use Transporters\TruckTransporter;
+
+class FactoryTest extends TestCase
+{
+    public function test_can_create_plane_transporter() {
+        $transporterFactory = new PlaneTransporterFactory();
+        $transporter = $transporterFactory->getTransporter();
+
+        $this->assertInstanceOf(PlaneTransporter::class, $transporter);
+
+    }
+    public function test_can_create_ship_transporter() {
+        $transporterFactory = new ShipTransporterFactory();
+        $transporter = $transporterFactory->getTransporter();
+
+        $this->assertInstanceOf(ShipTransporter::class, $transporter);
+
+    }
+    public function test_can_create_truck_transporter() {
+        $transporterFactory = new TruckTransporterFactory();
+        $transporter = $transporterFactory->getTransporter();
+
+        $this->assertInstanceOf(TruckTransporter::class, $transporter);
+
+    }
+}
